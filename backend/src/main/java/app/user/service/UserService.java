933b4e05992aa32c294378b5be6221db627b71c9
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-10-19 20:07:21 +0300

    add UserService

--- backend/src/main/java/app/user/service/UserService.java
+++ backend/src/main/java/app/user/service/UserService.java
@@ -1,8 +1,31 @@
 package app.user.service;
 
+import app.security.CustomUserDetails;
+import app.user.model.User;
+import app.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
 
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email " + username + " not found."));
+
+        return CustomUserDetails.builder()
+                .id(user.getId())
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .role(user.getRole())
+                .isActive(true)
+                .build();
+    }
 }
