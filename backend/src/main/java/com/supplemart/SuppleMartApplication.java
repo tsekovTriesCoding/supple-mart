5ad38aa4085c2542280ded8f85d73b1c3ed59372
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-10-19 16:44:46 +0300

    init backend proj

---
+++ backend/src/main/java/com/supplemart/SuppleMartApplication.java
@@ -0,0 +1,13 @@
+package com.supplemart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuppleMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuppleMartApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/supplemart/SuppleMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supplemart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuppleMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
