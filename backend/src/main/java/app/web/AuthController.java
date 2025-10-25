d85b11dfede793c0a681236da37d92b37a44f60f
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-10-25 15:00:26 +0300

    proper controller naming

--- backend/src/main/java/app/web/AuthController.java
+++ backend/src/main/java/app/web/AuthController.java
@@ -12,17 +12,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 @Slf4j
-public class UserController {
+public class AuthController {
 
     private final UserService userService;
     private final JwtService jwtService;
