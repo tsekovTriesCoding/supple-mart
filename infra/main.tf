0eafa97493276ac3ff9fafe610a23be466f08242
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-12-31 17:04:13 +0200

    feat: Add Terraform remote state backend using Azure Blob Storage

--- infra/main.tf
+++ infra/main.tf
@@ -4,6 +4,13 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "stsupplemartstate"
+    container_name       = "tfstate"
+    key                  = "supplemart.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
